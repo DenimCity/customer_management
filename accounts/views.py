874e5e1af16fbd46c6a23ce0ecfe0eb2edff1ba3
DenimCity <altidorj1@gmail.com>
2020-02-08 07:15:23 -0500

    added a template

--- accounts/views.py
+++ accounts/views.py
@@ -5,13 +5,15 @@ from django.http import HttpResponse
 
 
 def home(request):
-    print(request)
-    return HttpResponse('home')
+    print("Hi Its me OHHHHHHHH SNAP I HAVE A LOG")
+    return render(request, 'accounts/dashboard.html')
 
 
 def products(request):
     print(request)
     return HttpResponse('products')
+
+# This is and example of a backend api call
 
 
 def customer(request):
