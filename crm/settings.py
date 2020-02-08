83f94d96712f4019ec6489f8b7a100647d1fd0fb
DenimCity <altidorj1@gmail.com>
2020-02-08 16:34:48 -0500

    made css global

--- crm/settings.py
+++ crm/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    #put the name of the app in here so django knows about it
+
+    # put the name of the app in here so django knows about it
 
     'accounts'
 ]
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
