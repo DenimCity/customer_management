6b6196e911240740f5be6e0b3d4aa7712c323f08
DenimCity <altidorj1@gmail.com>
2020-02-09 08:58:24 -0500

    delete functionality works

--- accounts/views.py
+++ accounts/views.py
@@ -65,6 +65,15 @@ def updateOrder(request, pk):
     return render(request, 'accounts/order_form.html', context)
 
 
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    context = {'item': order}
+    return render(request, 'accounts/delete.html', context)
+
+
 # This is and example of a backend api call
 
 # def customer(request):
